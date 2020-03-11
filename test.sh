41ff063c2c82e69d988be847c4a45b3da01bfd3f
Fredrik Meringdal <fmeringdal@hotmail.com>
2020-03-11 14:36:46 +0100

    Deleted build and test shell scripts

--- test.sh
+++
@@ -1,9 +0,0 @@
-WEBSERVER="webserver"
-STORAGEENGINE="storageengine"
-
-projects=($WEBSERVER $STORAGEENGINE)
-
-for project in "${projects[@]}"
-do
-    cargo test --manifest-path "./projects/${project}/Cargo.toml"
-done
