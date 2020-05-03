f5ab76c9eee1effeeacf14bcb63638f5ef47fbd3
Fredrik Meringdal <fmeringdal@hotmail.com>
2020-05-03 20:22:13 +0200

    inifinite nesting of routes

--- projects/webserver/src/router/router.rs
+++ projects/webserver/src/router/router.rs
@@ -32,6 +32,28 @@ fn paths_match(route_path: &String, called_path: &String) -> bool {
     return true;
 }
 
+fn path_is_subpath(route_path: &String, called_path: &String) -> bool {
+    if *route_path == *called_path {
+        return true;
+    }
+
+    let route_path_dir: Vec<&str> = route_path.split("/").collect();
+    let called_path_dir: Vec<&str> = called_path.split("/").collect();
+    if route_path_dir.len() > called_path_dir.len() {
+        return false;
+    }
+
+    for i in 0..route_path_dir.len() {
+        let nested_route_path = String::from(route_path_dir[i]);
+        let nested_called_path = String::from(called_path_dir[i]);
+        if !nested_route_path.starts_with(":") && nested_route_path != nested_called_path {
+           return false;
+        }
+    }
+
+    return true;
+}
+
 pub struct Router {
     path: String,
     routes: Vec<Route>,
@@ -88,20 +110,21 @@ impl Router {
     }
 
     pub fn handle_request(&self, req: &mut Request, res: &mut Response, parent_path: &String) -> bool {
-        println!("Handle request for path: {}", req.path);
+        
+        let router_path = format!("{}{}", parent_path, self.path);
+        if !path_is_subpath(&router_path, &req.path) {
+            return false;
+        }
+
         // look through routes
         for route in &self.routes {
-            println!("Checking child path: {}", route.path);
-            let path = format!("{}{}{}", parent_path, self.path, route.path);
-            println!("With path: {}", path);
+            let path = format!("{}{}", router_path, route.path);
             if route.method == req.method &&
             paths_match(&path, &req.path) {
                 route.handler(req, res);
                 return true;
             }
         }
-
-        
 
         // look through childs
         for child_router in &self.childs {
